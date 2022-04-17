5e96d620fa4234e62acbc2bd120eb3731175c60e
Abrar051 <shahriar1410051@gmail.com>
2022-04-17 14:20:47 -0700

    if else

--- netapplication/Program.cs
+++ netapplication/Program.cs
@@ -13,10 +13,35 @@ namespace netapplication
             byte number = 2;
             int num = 4;
             string name = "Abrar";
+            /*
             Console.WriteLine(num);
             Console.WriteLine(number);
             Console.WriteLine("Starting");
             Console.WriteLine(name);
+            addNumber(); */
+            trueJudge(true);
         }
+
+        static void addNumber()
+        {
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a+b);
+            Console.ReadLine();
+        }
+
+        static void trueJudge(bool a)
+        {
+            if (a == true){
+                Console.WriteLine("It's true");
+            }
+            else if (a == false)
+            {
+                Console.WriteLine("It's false");
+            }
+        }
+
+       
     }
 }
