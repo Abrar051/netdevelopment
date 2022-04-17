8a8aa540a7b067d55bc12160de288ddbd4e41fb6
Abrar051 <shahriar1410051@gmail.com>
2022-04-17 12:38:50 -0700

    practicing c#

---
+++ netapplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace netapplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int num = 4;
+            string name = "Abrar";
+            Console.WriteLine(num);
+            Console.WriteLine(number);
+            Console.WriteLine("Starting");
+            Console.WriteLine(name);
+        }
+    }
+}
